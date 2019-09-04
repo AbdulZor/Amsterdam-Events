253866a996a591dc9d7aff91bfaabae4482e6893
zora <zora@hva.nl>
2019-09-04 14:40:02 +0200

    Assignment 3.1 done

--- Amsterdam-Events/src/app/app.module.ts
+++ Amsterdam-Events/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
